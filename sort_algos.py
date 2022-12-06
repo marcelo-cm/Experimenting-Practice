4f4a5cd35c2e971ae0db070b6abb42613124eb19
Marcelo Chaman Mallqui <marcechaman@gmail.com>
2022-12-06 01:42:45 -0500

    cleaning up code

--- sort_algos.py
+++ sort_algos.py
@@ -1,38 +1,41 @@
-def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        while i > 0 and arr[i] < arr[i - 1]:
-            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        while i > 0 and array[i] < array[i - 1]:
+            array[i], array[i - 1] = array[i - 1], array[i]
             i -= 1
-    return arr
+    return array
 
-def quick_sort(arr):
-    if len(arr) == 1:
-        return arr
-    if len(arr) == 0:
+
+def quick_sort(array):
+    if len(array) == 1:
+        return array
+    if len(array) == 0:
         return []
-    pivot = arr[len(arr)//2]
+    pivot = array[len(array) // 2]
     left_arr = []
-    right_arr =  []
-    for i in arr:
+    right_arr = []
+    for i in array:
         if i < pivot:
             left_arr.append(i)
         if i > pivot:
             right_arr.append(i)
     return quick_sort(left_arr) + [pivot] + quick_sort(right_arr)
 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
 
-    mid = len(arr)//2
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
 
-    left  = arr[:mid]
-    right = arr[mid:]
+    mid = len(array) // 2
+
+    left = array[:mid]
+    right = array[mid:]
 
     left_sorted = merge_sort(left)
     right_sorted = merge_sort(right)
 
-    return merge(left_sorted,right_sorted)
+    return merge(left_sorted, right_sorted)
+
 
 def merge(left, right):
     result = []
@@ -50,28 +53,31 @@ def merge(left, right):
 
     return result
 
-def bubble_sort(arr):
+
+def bubble_sort(array):
     swapped = False
-    for el in arr:
-        for i in range(len(arr)-1):
-            if arr[i] > arr[i+1]:
-                arr[i],arr[i+1] = arr[i+1], arr[i]
+    for el in array:
+        for i in range(len(array) - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
                 swapped = True
-        if swapped == False:
-            return arr
+        if not swapped:
+            return array
 
-def selection_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    min = None
+
+def selection_sort(array):
+    if len(array) <= 1:
+        return array
+    minimum = None
     min_idx = None
-    for i in range(len(arr)):
-        if min == None or arr[i] < min:
-            min =  arr[i]
+    for i in range(len(array)):
+        if not minimum or array[i] < minimum:
+            minimum = array[i]
             min_idx = i
-    return [min] + selection_sort(arr[:min_idx]+arr[min_idx+1:])
+    return [minimum] + selection_sort(array[:min_idx] + array[min_idx + 1:])
 
-arr =  [5,3,2,15,235]
+
+arr = [5, 3, 23, 15, 235]
 
 print(arr)
 insertion_sort(arr)
@@ -79,25 +85,25 @@ print(arr)
 
 print("----")
 
-arr =  [5,3,2,15,235]
+arr = [5, 3, 23, 15, 235]
 print(arr)
 print(quick_sort(arr))
 
 print("----")
 
-arr =  [5,3,2,15,235]
+arr = [5, 3, 23, 15, 235]
 print(arr)
 print(merge_sort(arr))
 
 print("----")
 
-arr =  [5,3,2,15,235]
+arr = [5, 3, 23, 15, 235]
 print(arr)
 bubble_sort(arr)
 print(arr)
 
 print("----")
 
-arr =  [5,3,2,15,235]
+arr = [5, 3, 23, 15, 235]
 print(arr)
 print(selection_sort(arr))
